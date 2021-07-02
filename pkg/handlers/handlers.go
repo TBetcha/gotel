747cf8c21e26fa7876d12ade8ebb865e0c26cb37
TMB <Troy.Boettger@gmail.com>
2021-07-02 06:33:56 -0400

    Introduce repository pattern

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,17 +1,37 @@
 package handlers
 
 import (
+	"github.com/tbetcha/gotel/pkg/config"
 	"github.com/tbetcha/gotel/pkg/render"
 	"net/http"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
-
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,36 +3,46 @@ package render
 import (
 	"bytes"
 	"fmt"
+	"github.com/tbetcha/gotel/pkg/config"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+var functions = template.FuncMap{}
 
-	tc, err := CreateTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+var app *config.AppConfig
+
+// NewTemplates sets the config for template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		//use tempalte cache
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from  template cache")
 	}
 
 	buf := new(bytes.Buffer)
 
 	_ = t.Execute(buf, nil)
 
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
 		fmt.Println("error writing template to browser: ", err)
 	}
 
 }
-
-var functions = template.FuncMap{}
 
 //CreateTemplateCache create temp cache as map
 func CreateTemplateCache() (map[string]*template.Template, error) {
